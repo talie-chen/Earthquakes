3852c3cbd770eb0ac9f6ab6b9a2cc9ca01c23e06
binerys <bree.nery@gmail.com>
2016-10-21 13:33:57 -0700

    Create JSON object array of earthquakes

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  $.get( "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson").done(function(data){
-  		console.log(data);
+
+	/**
+	* Earthquake Object
+	*	mag
+	*	place
+	*	time
+	*	coordinates[]
+	*/
+
+	var earthquake_list = [];
+
+	$.get( "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson").done(function(data)
+	{
+		// Building earthquakes list:
+		$.each(data.features, function(key, value){
+			var earthquake = {
+				"mag": value.properties.mag,
+				"place": value.properties.place,
+				"time": value.properties.time,
+				"coordinates": value.geometry.coordinates 
+			}
+			earthquake_list.push(earthquake);
+		});
+		console.log(earthquake_list);
 	});
 });
